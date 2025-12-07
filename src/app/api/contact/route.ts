41826fad6f939a59fb310d95b19d7e4f12a4c291
porto <yospunyaporto@gmail.com>
2025-12-08 00:42:17 +0700

    fix: fix null file

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest} from "next/server";
+
+export const GET = async () => {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+};
